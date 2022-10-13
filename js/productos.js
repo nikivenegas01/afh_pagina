28b180285225eeaff137a87795d26f7d559f2202
nikivenegas01 <nikivenegas01@gmail.com>
2022-10-12 20:05:48 -0500

    Ultimos cambios a la pagina

--- js/productos.js
+++ js/productos.js
@@ -1,32 +1,12 @@
-
 //Correr esta funcion cuando el documento este listo
 $(document).ready(function(){
-    //Crear accordion en el elemento #accSecciones
     $("#accSecciones").accordion({
         icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus"},
-        heightStyle: "content"
+        heightStyle: "content", collapsible: true
     })
 
-    /*$(".nuestrosproductos .prod-card > img").wrap(`<a class="imglink" data-lightbox="nuestrosproductos"></a>`)
-    const $links = $(".nuestrosproductos .prod-card > a")
-    console.log($links.children().first())
-    $links.attr("href", $links.children().first().attr("src"))*/
-
-    
-
-    /*const cards = $(".nuestrosproductos .prod-card")
-    cards.each((i, card)=> {
-      console.log(card)
-      
-      const img = $(card).get(0).clone()
-      $(card).get(0).remove()
-      $(card).prepend($(`<a class="imglink" data-lightbox="nuestrosproductos"></a>`))
-      $(card).get(0).prepend(img)
-    })*/
-    
-
-
-
-    
+    $("#accSecciones > h3").click(function(){
+      document.getElementById("nuestrosproductosh2").scrollIntoView({ behavior: 'smooth'});
+    })
 })
 
